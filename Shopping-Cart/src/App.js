ad2bee00c12fac8c1cf6ac72edfc89ef9ef1058c
Sabrina Senna <78413211+SabrinaLemeSenna@users.noreply.github.com>
2022-03-14 16:18:25 +0100

    add new files

--- Shopping-Cart/src/App.js
+++ Shopping-Cart/src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import Shop from './components/Shop'
+import Workshops from './components/Workshops'
+import Aboutus from './components/Aboutus'
+import Contact from './components/Contact'
 import Cart from './components/Cart'
 
 class App extends Component {
@@ -13,6 +17,10 @@ class App extends Component {
               <Navbar/>
                 <Switch>
                     <Route exact path="/" component={Home}/>
+                    <Route path="/shop" component={Shop}/>
+                    <Route path="/workshops" component={Workshops}/>
+                    <Route path="/aboutus" component={Aboutus}/>
+                    <Route path="/contact" component={Contact}/>
                     <Route path="/cart" component={Cart}/>
                   </Switch>
              </div>
