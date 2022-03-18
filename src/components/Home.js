b23e8cdf273790120a3fb256907a1cef3683fe5f
Sabrina Senna <78413211+SabrinaLemeSenna@users.noreply.github.com>
2022-03-18 16:39:17 +0100

    create mutliple sections for home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,59 @@
 import React from 'react';
 import Carousel from './Carousel';
 import Product from './Product';
+import HandmadePottery from '../images/pottery_handmade.jpg';
 import { Container } from 'react-bootstrap';
+import Workshop from './Workshop';
+import { Button } from 'react-bootstrap';
 
 const Home = () => {
     return(
         <Container>
             <Carousel />
             <Container className='best-seller-container'>
-                <h1>Best Seller</h1>
+                <Container className='title-container'>
+                    <h1>Best Seller</h1>
+                </Container>
+                <Container className='d-flex justify-content-center justify-content-between'>
+                        <Product />
+                        <Product />
+                        <Product />
+                </Container>
             </Container>
-            <Container className='d-flex justify-content-center justify-content-between'>
-                    <Product />
-                    <Product />
-                    <Product />
+            <Container className='handmade-container'>
+                <Container className='d-flex'>
+                    <Container>
+                        <Container className='title-handmade d-flex justify-content-end'>
+                            <h1>100% Handmade</h1>
+                        </Container>
+                        <Container className='text-handmade1'>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</p>
+                        </Container>
+                        <Container className='text-handmade2'>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        </Container>
+                    </Container>
+                    <img src={HandmadePottery}></img>
+                </Container>
+            </Container>
+            <Container className='workshops-container'>
+                <Container className='title-container'>
+                    <h1>Workshops & Events</h1>
+                </Container>
+                <Container className='workshop-container d-flex justify-content-center justify-content-between'>
+                    <Workshop />
+                    <Workshop />
+                    <Workshop />
+                </Container>
+                <Button variant="primary">Go somewhere</Button>
+            </Container>
+            <Container>
+                <Container className='title-container'>
+                    <h1>Behind the Scenes</h1>
+                </Container>
+                <Container className='behind-text'>
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+                </Container>
             </Container>
         </Container>
     )
---
+++ src/components/Workshop.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Workshop1 from '../images/workshop1.png';
+import { Card } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+
+const Workshop = () => {
+    return (
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={Workshop1} />
+            <Card.Body className='product-container'>
+                <Card.Title>Workshop</Card.Title>
+                <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    )
+}
+export default Workshop;
