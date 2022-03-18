5405d1cf0d1b12a2888e0e86db78ca5a45c60e21
Sabrina Senna <78413211+SabrinaLemeSenna@users.noreply.github.com>
2022-03-18 17:30:21 +0100

    commit section

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,12 @@ import React from 'react';
 import Carousel from './Carousel';
 import Product from './Product';
 import HandmadePottery from '../images/pottery_handmade.jpg';
+import Sabrina from '../images/sabrina.jpg';
 import { Container } from 'react-bootstrap';
 import Workshop from './Workshop';
 import { Button } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 
 const Home = () => {
     return(
@@ -51,9 +54,19 @@ const Home = () => {
                 <Container className='title-container'>
                     <h1>Behind the Scenes</h1>
                 </Container>
-                <Container className='behind-text'>
-                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
-                </Container>
+                <Row>
+                    <Col className='d-flex justify-content-center'>
+                        <img src={Sabrina}></img>
+                    </Col>
+                    <Col>
+                        <Row className='behind-text'>
+                            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+                        </Row>
+                        <Row className='signing'>
+                            <h2>Sabrina Senna</h2>
+                        </Row>
+                    </Col>
+                </Row>
             </Container>
         </Container>
     )
