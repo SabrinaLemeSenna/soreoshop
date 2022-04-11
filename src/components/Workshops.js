d595136225a34d43865b854192a81ee83cb3b428
Sabrina Senna <78413211+SabrinaLemeSenna@users.noreply.github.com>
2022-04-11 13:17:53 +0200

    commit

--- src/components/Workshops.js
+++ src/components/Workshops.js
@@ -1,50 +1,35 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import { addToCart } from './actions/cartActions'
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import Workshop from './Workshop';
 
- class Workshops extends Component{
-    handleClick = (id)=>{
-        this.props.addToCart(id); 
-    }
-    
-    render(){
-        // let itemList = this.props.items.map(item=>{
-        //     return(
-        //         <div className="card" key={item.id}>
-        //                 <div className="card-image">
-        //                     <img src={item.img} alt={item.title}/>
-        //                     <span className="card-title">{item.title}</span>
-        //                     <span to="/" className="btn-floating halfway-fab waves-effect waves-light red" onClick={()=>{this.handleClick(item.id)}}><i className="material-icons">add</i></span>
-        //                 </div>
+const Workshops = () => {
+    return (
+        
+        <Container className='workshop-page-container'>
+            <h1>Workshops &amp; Events</h1>
+            <Row>
+                <Col d-flex justify-content-end>
+                    <Workshop>
+                        Hello
+                    </Workshop>
+                </Col>
+                <Col>
+                    <p>HELLOOOO</p>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Workshop>
+                        Hello
+                    </Workshop>
+                </Col>
+                <Col>
+                    <p>HELLOOOO</p>
+                </Col>
+            </Row>
+        </Container>
+    )
 
-        //                 <div className="card-content">
-        //                     <p>{item.desc}</p>
-        //                     <p><b>Price: {item.price}$</b></p>
-        //                 </div>
-        //          </div>                           
-
-        //     )
-        // })
-
-        return(
-            <div className="container">
-                Workshops
-            </div>
-        )
-    }
-
-    
-}
-const mapStateToProps = (state)=>{
-    return {
-      items: state.items
-    }
-  }
-const mapDispatchToProps= (dispatch)=>{
-    
-    return{
-        addToCart: (id)=>{dispatch(addToCart(id))}
-    }
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(Workshops)
+export default Workshops;
